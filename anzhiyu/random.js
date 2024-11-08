6a4337cadd2fbb82ccb86231b4bbcf911ebda769
weiyuhl <weiyuhl@users.noreply.github.com>
2024-11-08 08:33:04 +0000

    deploy: weiyuhl/hexo@23a76a344ed0c6a7f518d8e5af0ba77a24b206ed

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/08/hello-world/"];function toRandomPost(){
+var posts=["2024/11/08/hello-world/","2024/11/08/川渝暴龙/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
